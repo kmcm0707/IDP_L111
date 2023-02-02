0f75a19d3af58095eb093d9af2af5af3f891ea99
kmc0707 <kmc0707@hotmail.com>
2023-02-02 04:06:00 +0000

    sever working

--- server/main.py
+++ server/main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 import json
+import requests
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return {"res": 10}
+@app.route("/data", methods=['POST', 'GET'])
+def data():
+    #dictToReturn = {'answer':42}
+    #return jsonify(dictToReturn)
+    return "Hello World!"
 
-
-app.run()
+if __name__ == '__main__':
+    app.run()
+# $ flask run --host=0.0.0.0
+# flask --app main run --host=0.0.0.0
+# POST http://localhost:5000/data
+# GET http://localhost:5000/data
+# POST http://128.232.250.181:5000/data
