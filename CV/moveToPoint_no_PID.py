0ed2ea23f43638e2ef03102e710e609f38a4afba
Devamitra Acharya <da533@cam.ac.uk>
2023-02-08 14:30:59 +0000

    added CV navigation

--- CV/moveToPoint_no_PID.py
+++ CV/moveToPoint_no_PID.py
@@ -13,6 +13,8 @@ import paho.mqtt.client as mqtt
 import os
 import json
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 # import requests
 from sys import platform
 import math
@@ -51,6 +53,8 @@ mqttBroker = "test.mosquitto.org"
 # mqttBroker =  "public.mqtthq.com"
 client = mqtt.Client("Python")
 client.connect(mqttBroker)
+
+global target
 
 
 class VideoGet:
@@ -127,6 +131,131 @@ def perspective_transoformation(img, dim):
     return M
 
 
+def rotate(
+    video_getter,
+    M,
+    target,
+    current_position,
+    detector,
+    mtx,
+    dist,
+    newcameramtx,
+    dim,
+    current_head=None,
+    fix_distortion=True,
+    fix_perspective=True,
+):
+    v = target - current_position
+    v /= np.linalg.norm(v)
+    current_head /= np.linalg.norm(current_head)
+    angle_diff = np.artann2(np.dot(v, current_head))
+    if angle_diff < 0:
+        client.publish("IDP_2023_Follower_left_speed", -200)
+        client.publish("IDP_2023_Follower_right_speed", 200)
+    else:
+        client.publish("IDP_2023_Follower_left_speed", 200)
+        client.publish("IDP_2023_Follower_right_speed", -200)
+
+    while True:
+        frame = video_getter.frame
+        # frame = detect_red(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        """if not ret:
+            print("no frame retereived; trying apain")
+            continue"""
+
+        if fix_distortion:
+            frame = cv2.undistort(frame, mtx, dist, None, newcameramtx)
+
+        if fix_perspective:
+            frame = cv2.warpPerspective(frame, M, dim)
+
+        result = detector(frame)
+        if len(result) > 0:
+            if first_time:
+                first_time = False
+                continue
+
+            current_position[:] = result[0].center
+            print("target", target)
+            print("current", current_position)
+            heading = np.float64(target - current_position)
+            heading /= np.linalg.norm(heading)
+            v = np.float64(result[0].corners[1] - result[0].corners[0])
+            v /= np.linalg.norm(v)
+            angle_diff = np.arccos(np.dot(v, heading))
+
+            print("angle: ", angle_diff)
+
+            prev_time = time.time()
+
+            prev_angle = angle_diff
+
+            if angle_diff < 0.1:
+                return
+
+            a, b, c, d = result[0].corners
+            print(current_position)
+            frame = cv2.circle(
+                frame,
+                current_position.astype("int32"),
+                radius=12,
+                color=(255, 255, 255),
+                thickness=-1,
+            )
+            frame = cv2.polylines(
+                frame, [np.int32(result[0].corners)], True, (255, 255, 255), 2
+            )
+
+        cv2.imshow("frame", frame)
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            return
+
+
+def forward(time=2):
+    client.publish("IDP_2023_Follower_left_speed", 200)
+    client.publish("IDP_2023_Follower_right_speed", 200)
+    time.sleep(time)
+
+
+def get_points(img):
+    """function for manual perspective transformation of an image
+    returns the transformation matrix"""
+    points = []
+
+    def click_envent(event, x, y, flags, params):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            print(x, y)
+            cv2.circle(img, (x, y), 2, (0, 0, 255), -1)
+            points.append((x, y))
+
+    cv2.imshow("img", img)
+    cv2.setMouseCallback("img", click_envent)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    points = np.int32(points)
+    return points
+
+
+"""def controll():
+    src = "http://localhost:8081/stream/video.mjpeg"
+    video_getter = VideoGet(src).start()
+
+    detector = apriltag.Detector()
+    mtx, dist, newcameramtx, dim = cal.load_vals(6)
+    M = perspective_transoformation(video_getter.frame, dim)
+    mtx, dist, newcameramtx, dim = cal.load_vals(6)
+    current_head = np.float64([1, 0])
+
+    # points = get_points(video_getter.frame)
+    # M = cv2.getPerspectiveTransform(points, new_points)   
+        current_position = np.float64(result[0].center)
+        rotate(video_getter, M, point, detector, mtx, dist, newcameramtx, dim, current_head)
+        forward(2)"""
+
+
 def apriltag_detector_procedure(
     src, module, fix_distortion=True, fix_perspective=True, alpha=1, controller=None
 ) -> None:
@@ -200,11 +329,7 @@ def apriltag_detector_procedure(
         except:
             raise ValueError("need a valid module")
 
-    prev_point = np.array([0, 0])
     current_position = np.array([0, 0])
-    interval = 0
-    prev_time = time.time()
-    positions = []
     first_time = True
     frame = video_getter.frame
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -213,19 +338,28 @@ def apriltag_detector_procedure(
 
     if fix_perspective:
         frame = cv2.warpPerspective(frame, M, dim)
+    global target
+    target = None
 
     def click_envent(event, x, y, flags, params):
         if event == cv2.EVENT_LBUTTONDOWN:
+            global target
             print(x, y)
-            controller.set_target_position(np.array([x, y]))
+            target = np.array([x, y])
             cv2.destroyAllWindows()
-            positions.append(np.array([x, y]))
 
     cv2.imshow("img", frame.copy())
     cv2.setMouseCallback("img", click_envent)
     key = cv2.waitKey(0)
     print("hello")
 
+    frame_counter = 0
+    client.publish("IDP_2023_Follower_left_speed", -100)
+    client.publish("IDP_2023_Follower_right_speed", 100)
+
+    # current_position = np.array([0, 0])
+    moving_forward = False
+    clockwise = True
     while True:
         frame = video_getter.frame
         # frame = detect_red(frame)
@@ -247,27 +381,58 @@ def apriltag_detector_procedure(
                 first_time = False
                 continue
 
-            x, y = result[0].center
-            # theta = angle(result[0])
+            current_position[:] = result[0].center
+            print("target", target)
+            print("current", current_position)
+            heading = np.float64(target - current_position)
+            heading /= np.linalg.norm(heading)
+            v = np.float64(result[0].corners[1] - result[0].corners[0])
+            v /= np.linalg.norm(v)
+            angle_diff = np.arccos(np.dot(v, heading))
 
-            current_position[:] = [x, y]
-            controller.set_current_position(current_position)
+            print("angle diference: ", angle_diff)
 
-            positions.append(current_position)
+            if np.linalg.norm(current_position - target) < 10:
+                client.publish("IDP_2023_Follower_left_speed", 0)
+                client.publish("IDP_2023_Follower_right_speed", 0)
+                print("done")
+                break
 
-            interval = time.time() - prev_time
-            speed = (result[0].center - prev_point) / interval
-            current_position += np.int64(speed * interval * 100)
-            controller.set_predicted_position(current_position)
-            controller.PID_controller_update()
-            prev_time = time.time()
+            if abs(angle_diff) < 0.2:
+                if not moving_forward:
+                    client.publish("IDP_2023_Follower_left_speed", 250)
+                    client.publish("IDP_2023_Follower_right_speed", 250)
+                    moving_forward = True
+                    print("forward")
 
-            prev_point = result[0].center
+            else:
+                if (heading[0] - np.float64(current_position[0])) * (
+                    v[1] - np.float64(current_position[1])
+                ) - (heading[1] - np.float64(current_position[1])) * (
+                    v[0] - np.float64(current_position[0])
+                ) > 0:
+                    if not clockwise:
+                        client.publish("IDP_2023_Follower_left_speed", -100)
+                        client.publish("IDP_2023_Follower_right_speed", 100)
+                        clockwise = True
+                        print("clockwise")
+                else:
+                    if clockwise:
+                        client.publish("IDP_2023_Follower_left_speed", 100)
+                        client.publish("IDP_2023_Follower_right_speed", -100)
+                        print("anticlockwise")
+                        clockwise = False
 
-            a, b, c, d = result[0].corners
-            frame = cv2.circle(
-                frame, (int(x), int(y)), radius=10, color=(0, 0, 255), thickness=-1
-            )
+                """if angle_diff > 0:
+                    client.publish("IDP_2023_Follower_left_speed", -100)
+                    client.publish("IDP_2023_Follower_right_speed", 100)
+                    print("clockwise")
+                else:
+                    client.publish("IDP_2023_Follower_left_speed", 100)
+                    client.publish("IDP_2023_Follower_right_speed", -100)
+                    print("anticlockwise")"""
+
+            """a, b, c, d = result[0].corners
             print(current_position)
             frame = cv2.circle(
                 frame,
@@ -279,7 +444,6 @@ def apriltag_detector_procedure(
             frame = cv2.polylines(
                 frame, [np.int32(result[0].corners)], True, (255, 255, 255), 2
             )
-            print(speed)
             if first_time:
                 frame = cv2.polylines(
                     frame, [np.int32(positions)], False, (0, 0, 255), 2
@@ -287,7 +451,7 @@ def apriltag_detector_procedure(
                 first_time = False
 
         if not first_time:
-            frame = cv2.polylines(frame, [np.int32(positions)], False, (0, 0, 255), 2)
+            frame = cv2.polylines(frame, [np.int32(positions)], False, (0, 0, 255), 2)"""
 
         cv2.imshow("frame", frame)
         key = cv2.waitKey(1)
@@ -305,86 +469,18 @@ def apriltag_detector_procedure(
     cv2.destroyAllWindows()
 
 
-class PID:
-    def __init__(self):
-        self.current_position = np.array([0, 0])
-        self.target_position = np.array([0, 0])
-        self.predicted_position = np.array([0, 0])
-        self.elapsed_time = time.time()
-
-    def get_right_speed(self):
-        return self.right_speed
-
-    def get_left_speed(self):
-        return self.left_speed
-
-    def set_current_position(self, current_position):
-        self.current_position[:] = current_position
-
-    def set_target_position(self, target_position):
-        self.target_position[:] = target_position
-
-    def set_predicted_position(self, predicted_position):
-        self.predicted_position[:] = predicted_position
-
-    def PID_controller_update(self):
-        basespeed = 170
-        """This function will return the error for the PID controller"""
-        target = self.target_position - self.current_position
-        current_heading = None
-
-        heading_diff = (
-            np.dot(target, current_heading) / np.norm(target) * np.norm(current_heading)
-        )
-
-        if velocityAngle == 2 * math.pi or targetAngle == 2 * math.pi:
-            velocityAngle = 0
-            targetAngle = 0
-
-        temp_error = targetAngle - velocityAngle
-        if temp_error > math.pi or (temp_error < 0 and temp_error > -math.pi):
-            # turn right - left faster
-            temp_error = -abs(temp_error)
-        else:
-            # turn left - right faster
-            temp_error = abs(temp_error)
-        self.error = temp_error
-
-        self.integral = self.integral + self.error
-        D = self.error - self.prev_error
-        self.prev_error = self.error
-
-        motorspeed = int(self.kp * self.error + self.kd * D + self.ki * self.integral)
-        self.left_speed = basespeed - motorspeed
-        self.right_speed = basespeed + motorspeed
-        if time.time() - self.elapsed_time > 0.2:
-            self.elapsed_time = time.time()
-            self.publish()
-
-    def publish(self):
-        client.publish("IDP_2023_Follower_left_speed", str(self.left_speed))
-        client.publish("IDP_2023_Follower_right_speed", str(self.right_speed))
-        print(f"right : {self.right_speed}")
-        print(f"left : {self.left_speed}")
-
-
-def start_everything():
-    controller = PID()
+def main():
     if platform == "win32":
         # Windows
         apriltag_detector_procedure(
-            "http://localhost:8081/stream/video.mjpeg",
-            module=pupil_apriltags,
-            controller=controller,
+            "http://localhost:8081/stream/video.mjpeg", module=pupil_apriltags
         )
     else:
         # mac
         apriltag_detector_procedure(
-            "http://localhost:8081/stream/video.mjpeg",
-            module=apriltag,
-            controller=controller,
+            "http://localhost:8081/stream/video.mjpeg", module=apriltag
         )
 
 
 if __name__ == "__main__":
-    start_everything()
+    main()
